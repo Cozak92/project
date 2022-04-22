0a5c6534a5accdae4c14f8cfb07d3fc3f9f17824
신승혁 <cozak@sellmate.co.kr>
2022-04-22 10:18:40 +0900

    :tada: 과제 시작

---
+++ src/main/kotlin/com/ably/assignment/AssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.ably.assignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssignmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/ably/assignment/AssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ably.assignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
