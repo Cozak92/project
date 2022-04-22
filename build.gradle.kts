e536152dfbc7374aa5e0ad95da3fdc9ca2914503
신승혁 <cozak@sellmate.co.kr>
2022-04-22 17:42:54 +0900

    :heavy_plus_sign: validation,mapstruct,libphonenumber,kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,21 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+	kotlin("kapt") version "1.4.10"
+	idea
+	jacoco
 }
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+noArg {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
 
 group = "com.ably"
 version = "0.0.1-SNAPSHOT"
@@ -19,13 +33,32 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("mysql:mysql-connector-java")
+
+	implementation("org.mapstruct:mapstruct:1.4.2.Final")
+	kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
+	implementation("com.googlecode.libphonenumber:libphonenumber:5.5")
+
+	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("io.kotest:kotest-runner-junit5:5.0.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.0.2")
+	testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
+
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.withType<KotlinCompile> {
