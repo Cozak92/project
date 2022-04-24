4bb62368238d536c9605f390a00459fed7c3e81d
신승혁 <Cozak92@gmail.com>
2022-04-24 23:44:38 +0900

    :construction: Spring Boot 필요한 어노테이션 추가

--- src/main/kotlin/com/ably/assignment/AssignmentApplication.kt
+++ src/main/kotlin/com/ably/assignment/AssignmentApplication.kt
@@ -2,8 +2,12 @@ package com.ably.assignment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 class AssignmentApplication
 
 fun main(args: Array<String>) {
