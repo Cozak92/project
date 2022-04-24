b4eb2c6d7b61b4f4cbc460c166a4190a2349b4a4
신승혁 <Cozak92@gmail.com>
2022-04-24 23:26:49 +0900

    :coffin: 필요 없는 코드 삭제

--- src/main/kotlin/com/ably/assignment/domain/port/outbound/UserOutBoundPort.kt
+++
@@ -1,8 +0,0 @@
-package com.ably.assignment.domain.port.outbound
-
-import com.ably.assignment.domain.entity.User
-
-interface UserOutBoundPort {
-    fun save(user:User): User
-    fun findByEmail(email: String): User?
-}
