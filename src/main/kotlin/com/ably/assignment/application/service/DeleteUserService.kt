5cbfecbd815e22ab8ecd8d4e86be249962976fee
신승혁 <Cozak92@gmail.com>
2022-04-27 21:54:57 +0900

    :card_file_box: DeleteUserService.kt @Transational 추가

--- src/main/kotlin/com/ably/assignment/application/service/DeleteUserService.kt
+++ src/main/kotlin/com/ably/assignment/application/service/DeleteUserService.kt
@@ -4,9 +4,12 @@ import com.ably.assignment.application.port.persistence.DeleteOutBoundPort
 import com.ably.assignment.application.usecase.DeleteUserUseCase
 import com.ably.assignment.domain.model.User
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class DeleteUserService(private val deleteOutBoundPort: DeleteOutBoundPort): DeleteUserUseCase {
+
+    @Transactional
     override fun deleteById(userId: Long) {
         deleteOutBoundPort.deleteById(userId)
     }
